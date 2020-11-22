56cbd8d6692cbc9b4119b45e39e6a96811bd31f1
ttt <tschachtli.th@gmail.com>
2020-11-22 17:26:24 +0100

    first

---
+++ src/main/java/ch/ttt/springbootdeploy/SpringBootDeployApplication.java
@@ -0,0 +1,13 @@
+package ch.ttt.springbootdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/ttt/springbootdeploy/SpringBootDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ttt.springbootdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
